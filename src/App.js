0d7b7eeb9b8e7c7c09e0a214e2701c9ae5399e34
vusimb313 <vusimb@realnet.co.sz>
2020-08-18 14:59:08 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Home from "./components/";
+import Resume from "./components/Resume"
+
+function App() {
+  return (
+    <>
+    <CssBaseline/>
+    <Route exact path="/" component={Home}/>
+    <Route path="/resume" component={Resume}/>
+
+    </>
+    
+  );
+}
+
+export default App;
